7e24fb49a3ef41b9dea3e13e21fe765e63d47b77
Shreyas <skraman1999@gmail.com>
2014-10-17 22:51:47 -0700

    Added permissions, moved bluetooth stuff to activity.

--- app/src/main/java/com/sparkapp/spark/MainActivity.java
+++ app/src/main/java/com/sparkapp/spark/MainActivity.java
@@ -5,17 +5,31 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 
 public class MainActivity extends Activity {
+
+    public static final UUID uuid = UUID.fromString("b042294f-84f1-43ca-b7f5-a84631b084f7");
 
     private boolean initBlue() {
         BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
@@ -59,5 +73,54 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void startDiscovery() {
+        final BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+        if (adapter == null) {
+            Log.e("BLUETOOTH", "NO BLUETOOTH?!?!?!");
+        }
+        Intent enableBluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(enableBluetoothIntent, 0);
+
+        final List<BluetoothDevice> devices = new ArrayList<BluetoothDevice>();
+
+        BroadcastReceiver foundReciever = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                if (BluetoothDevice.ACTION_FOUND.equals(intent.getAction()))
+                    devices.add((BluetoothDevice) intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE));
+            }
+        };
+        IntentFilter foundFilter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(foundReciever, foundFilter);
+
+        BroadcastReceiver finishedReciever = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                adapter.cancelDiscovery();
+            }
+        };
+        IntentFilter finishedFilter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(finishedReciever, finishedFilter);
+
+        adapter.startDiscovery();
+
+
+        List<BluetoothSocket> sockets = new ArrayList<BluetoothSocket>();
+        BluetoothServerSocket serverSocket = null;
+        try {
+            serverSocket = adapter.listenUsingRfcommWithServiceRecord(adapter.getName(), uuid);
+        } catch (IOException e) {
+            Log.e("BLUETOOTH", "ERROR: " + e.toString());
+        }
+
+        try {
+            while (true) {
+                BluetoothSocket socket = serverSocket.accept(10);
+                sockets.add(socket);
+            }
+        } catch (IOException e) {
+            Log.e("BLUETOOTH", "ERROR: " + e.toString());
+        }
+
     }
 }
